9d2a1d099091690e7503a66c4ab5b33b3b53b710
emre_gormus <yunus.gormus@trendyol.com>
2022-08-29 13:31:43 +0300

    add rsa keypair and attach

--- main.tf
+++ main.tf
@@ -13,10 +13,24 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_network_interface_sg_attachment" "sg_attachment" {
+resource "tls_private_key" "oskey" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "myterrakey" {
+  content  = tls_private_key.oskey.private_key_pem
+  filename = "myterrakey.pem"
+}
+
+resource "aws_key_pair" "key121" {
+  key_name   = "myterrakey"
+  public_key = tls_private_key.oskey.public_key_openssh
+}
+
+/* resource "aws_network_interface_sg_attachment" "sg_attachment" {
   security_group_id    = "sg-026ff8cfda7753a07"
   network_interface_id = "eni-0496c10c0f1f159a8"
-}
+} */
 
 resource "aws_instance" "kubernetes" {
   # Creates two identical aws ec2 instances for kubernetes
@@ -25,10 +39,19 @@ resource "aws_instance" "kubernetes" {
   # All four instances will have the same ami and instance_type
   ami = "ami-08d4ac5b634553e16"
   instance_type = "t2.micro"
-  key_name = "emre_aws_kubernetes"
-  /* security_group_id = "sg-026ff8cfda7753a07" */
+  key_name      = aws_key_pair.key121.key_name
+  /* key_name = "emre_aws_kubernetes" */
+
 
   tags = {
     Name = "kube-${count.index}"
   }
+
+  connection {
+    type        = "ssh"
+    user        = var.INSTANCE_USERNAME
+    private_key = tls_private_key.oskey.private_key_pem
+    host        = aws_instance.mytfinstance.public_ip
+  }
+
 }
---
+++ vars.tf
@@ -0,0 +1,3 @@
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"  # username which will be used while doing remote-execution with the launched instance.
+}
